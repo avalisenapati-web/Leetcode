b3e5be9814aa8293770493705140b386da7b62bb
Avalideepti senapti <senapatiavali@gmail.com>
2026-07-11 12:29:41 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 64.26%] - Leet2Hub

---
+++ 02-Two-Pointers/876. Middle of the Linked List/876. Middle of the Linked List.cpp
@@ -0,0 +1,23 @@
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* middleNode(ListNode* head) {
+14        ListNode* slow=head;
+15        ListNode* fast=head;
+16        while(fast!=NULL && fast->next!=NULL){
+17            slow=slow->next;
+18            fast=fast->next->next;
+19        }
+20        return slow;
+21        
+22    }
+23};
