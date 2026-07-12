48685cc08ba57ba43de94921d8ff3a64705623dd
Avalideepti senapti <senapatiavali@gmail.com>
2026-07-12 12:30:59 +0530

    [152] [Time Beats: 100.00%] [Memory Beats: 99.50%] - Leet2Hub

--- 01-Arrays-and-Hashing/152. Maximum Product Subarray/152. Maximum Product Subarray.cpp
+++ 01-Arrays-and-Hashing/152. Maximum Product Subarray/152. Maximum Product Subarray.cpp
@@ -1,14 +1,20 @@
 1class Solution {
 2public:
 3    int maxProduct(vector<int>& nums) {
-4        int n=nums.size();
-5        int currprod=nums[0];
-6        int maxprod=nums[0];
-7        for(int i=1;i<n;i++){
-8            currprod=max(nums[i],nums[i]*currprod);
-9            maxprod=max(currprod,maxprod);
-10        }
-11        return maxprod;
-12        
-13    }
-14};
+4       int n=nums.size();
+5       int maxprod=nums[0];
+6       int minprod=nums[0];
+7       int ans=nums[0];
+8       for(int i=1;i<n;i++){
+9        if(nums[i]<0){
+10            swap(maxprod,minprod);
+11
+12        }
+13        maxprod=max(nums[i],maxprod*nums[i]);
+14        minprod=min(nums[i],minprod*nums[i]);
+15        ans=max(maxprod,ans);
+16
+17       }
+18    return ans;
+19    }
+20};
