6b6f0d9288a54cc277b2978b75b3e2fb5bb794f0
Avalideepti senapti <senapatiavali@gmail.com>
2026-07-30 11:43:08 +0530

    [1539] [Time Beats: 100.00%] [Memory Beats: 29.47%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/1539. Kth Missing Positive Number/1539. Kth Missing Positive Number.cpp
@@ -0,0 +1,19 @@
+1class Solution {
+2public:
+3    int findKthPositive(vector<int>& arr, int k) {
+4        
+5        int low=0;
+6        int high=arr.size()-1;
+7        while(low<=high){
+8            int mid=low+(high-low)/2;
+9            int missing=arr[mid]-(mid+1);
+10            if(missing<k){
+11                low=mid+1;
+12            }else{
+13                high=mid-1;
+14
+15            }
+16        }
+17        return low+k;
+18    }
+19};
